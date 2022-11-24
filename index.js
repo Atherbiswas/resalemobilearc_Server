3a991896aa8c6fac3e4f64f14be8c5fac02f4638
Ather1230 <ather.biswas0057@gmail.com>
2022-11-24 23:38:47 +0600

    categories server loaded

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECURITY_KEY}@cluster0.ukuxru5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const categoryCollection = client.db('resalemobilearc').collection('brandcategory');
+
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
     res.send('resalemobilearc server is running')
